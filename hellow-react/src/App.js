0a1dd434143720938812cd315c5ba5ae7f3007d2
achmaddhavi12 <chumaedidhavi@gmail.com>
2025-03-21 20:40:05 -0700

    pertemuan 2

---
+++ hellow-react/src/App.js
@@ -0,0 +1,20 @@
+// import Libraries
+import React from "react";
+import Header from "./Header.jsx";
+
+// Membuat komponen dengan class
+
+
+// Create Component
+const App = () => {
+  return (
+    <div>
+      <h1>Hllo React</h1>
+      <p>Nama saya Dhavi</p>
+      <Header />
+    </div>
+  );
+};
+
+// Export Default
+export default App;
